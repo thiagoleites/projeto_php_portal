f4f5cce42bf9bfaf8e44e38449642985bca4157f
Thiago Leite <thiagoleites@gmail.com>
2025-05-25 12:47:40 -0300

    Feat: Adição de comentários de propriedade

--- core/Database/QueryBuilder.php
+++ core/Database/QueryBuilder.php
@@ -4,7 +4,7 @@
  * Project:     Sistema personalizado em PHP
  * Author:      Thiago Leite - Devt Digital
  * License:     Proprietary - Todos os direitos reservados
- * File:        DashboardController.php
+ * File:        QueryBuilder.php
  * Description: Classe responsável pela construção de queries SQL
  * ---------------------------------------------------------------------
  * Copyright (c) 2025 Devt Digital
@@ -14,18 +14,20 @@
 
 declare(strict_types=1);
 
-namespace App\Controllers\Admin;
+namespace Core\Database;
 
-use Core\View;
+use mysqli;
 
-class DashboardController
+class QueryBuilder
 {
-    public function index()
+    protected mysqli $conexao;
+    protected ?string $tabela;
+    protected ?array $bindings = [];
+    protected ?string $query;
+
+    public function __construct(mysqli $conexao, string $tabela)
     {
-        
-        View::render('admin/dashboard', [
-            'titulo'    => 'Dashboard - Painel de controle',
-            'conteudo'  => 'Bem-vindo ao painel de controle!',
-        ]);
+        $this->conexao = $conexao;
+        $this->tabela = $tabela;
     }
 }
