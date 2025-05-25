51686c23bb57aa7cd2d6250cff2eeb7050ad4f67
Thiago Leite <thiagoleites@gmail.com>
2025-05-25 18:01:57 -0300

    Commit assinado

--- core/View.php
+++ core/View.php
@@ -16,30 +16,6 @@ declare(strict_types=1);
 
 namespace Core;
 
-// class View
-// {
-//     private $view;
-//     private $data = [];
-
-//     public function __construct($view)
-//     {
-//         $this->view = $view;
-//     }
-
-//     public function assign($key, $value)
-//     {
-//         $this->data[$key] = $value;
-//     }
-
-//     public function render()
-//     {
-//         extract($this->data);
-//         ob_start();
-//         include "../app/Views/{$this->view}.php";
-//         return ob_get_clean();
-//     }
-// }
-
 class View
 {
     public static function render(string $viewPath, array $data = [])
