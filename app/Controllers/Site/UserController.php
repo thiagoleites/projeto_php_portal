0c154af2c1b29e36401c5b7f6bf816f97680f5bf
Thiago Leite <thiagoleites@gmail.com>
2025-05-30 01:40:55 -0300

    wip

---
+++ app/Controllers/Site/UserController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * ---------------------------------------------------------------------
+ * Project:     Sistema personalizado em PHP
+ * Author:      Thiago Leite - Devt Digital
+ * License:     Proprietary - Todos os direitos reservados
+ * File:        UserController.php
+ * Description: Classe responsável pela construção de queries SQL
+ * ---------------------------------------------------------------------
+ * Copyright (c) 2025 Devt Digital
+ * Thiago Leite <tls@devt.emp.br>
+ * ---------------------------------------------------------------------
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Site;
+
+
+class UserController
+{
+    public function index()
+    {
+        echo "Método index";
+    }
+
+    public function show()
+    {
+        echo "Método show";
+    }
+
+    public function profile()
+    {
+        echo "Método profile";
+    }
+
+        public function store()
+    {
+        echo "Método store";
+    }
+
+        public function update()
+    {
+        echo "Método update";
+    }
+}
