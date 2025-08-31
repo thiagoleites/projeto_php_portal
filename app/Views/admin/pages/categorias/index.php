3a0409cd1ab12ec910153848d42d1938c154cf1f
Thiago Leite <thiagoleites@gmail.com>
2025-08-31 03:13:42 -0300

    wip

--- app/Views/admin/pages/categorias/index.php
+++ app/Views/admin/pages/categorias/index.php
@@ -16,6 +16,8 @@ View::start('content');
  */
 
 ?>
+<!-- notificações -->
+<!--<div id="notification-container" class="fixed top-4 right-4 z-50 space-y-2 hidden"></div>-->
 
 <!-- Sistema de Notificações para mensagens de sessão -->
 <?php if (Helpers::session('sucesso')): ?>
