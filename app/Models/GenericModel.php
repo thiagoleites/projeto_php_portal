5899fc7ae3731391ef4718f29b83b72bbd63a869
Thiago Leite <thiagoleites@gmail.com>
2025-08-31 22:26:12 -0300

    Criacao do model Generico para auxiliar na execucao do CRUD

---
+++ app/Models/GenericModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database\Model;
+
+class GenericModel extends Model
+{
+    protected $table;
+
+    public function __construct(string $table)
+    {
+        $this->table = $table;
+        parent::__construct();
+    }
+
+    public function setTable(string $table): self
+    {
+        $this->table = $table;
+        return $this;
+    }
+}
