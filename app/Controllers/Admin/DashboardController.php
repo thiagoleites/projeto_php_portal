f6f3a5ea1aad2d48c96b69f4d6a9f5fd1704f1db
Thiago Leite <thiagoleites@gmail.com>
2026-01-08 16:23:10 +0000

    chore: remove php header from one file

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * ---------------------------------------------------------------------
- * Project: Sistema personalizado em PHP
- * Author: Thiago Leite - Devt Digital
- * License: Proprietary - Todos os direitos reservados
- * File: DashboardController.php
- * Description: Classe responsável pela construção de queries SQL
- * ---------------------------------------------------------------------
- * Copyright © 2025 Devt Digital
- * Thiago Leite <tls@devt.emp.br>
- * ---------------------------------------------------------------------
- */
-
 declare(strict_types=1);
 
 namespace App\Controllers\Admin;
