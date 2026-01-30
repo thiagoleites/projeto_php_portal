3806174149cb533e1f27d77b22b7a7be9b5ade8e
Thiago Leite <thiagoleites@gmail.com>
2026-01-30 16:32:21 +0000

    chore: remove php header from one file

--- core/Exceptions/QueryException.php
+++ core/Exceptions/QueryException.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * ---------------------------------------------------------------------
- * Project: Sistema personalizado em PHP
- * Author: Thiago Leite - Devt Digital
- * License: Proprietary - Todos os direitos reservados
- * File: DatabaseException.php
- * Description: Classe responsável pela construção de queries SQL
- * ---------------------------------------------------------------------
- * Copyright (c) 2025 Devt Digital
- * Thiago Leite <tls@devt.emp.br>
- * ---------------------------------------------------------------------
- */
 declare(strict_types=1);
 
 namespace Core\Exceptions;
