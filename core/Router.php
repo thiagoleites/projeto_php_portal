d5713ed49bdd85c6d0c4b3dda37e8e277eea0fcc
Thiago Leite <thiagoleites@gmail.com>
2026-02-04 16:40:00 +0000

    chore: remove php header from one file

--- core/Router.php
+++ core/Router.php
@@ -1,18 +1,4 @@
 <?php
-
-/**
- * ---------------------------------------------------------------------
- * Project: Sistema personalizado em PHP
- * Author: Thiago Leite - Devt Digital
- * License: Proprietary - Todos os direitos reservados
- *
- * Description: Classe responsável pela construção de queries SQL
- * ---------------------------------------------------------------------
- * Copyright © 2025 Devt Digital
- * Thiago Leite <tls@devt.emp.br>
- * ---------------------------------------------------------------------
- */
-
 declare(strict_types=1);
 
 namespace Core;
