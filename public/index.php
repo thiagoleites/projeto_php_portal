8ad782a39bf450e5c7cce7928684ad4cbb70c0e3
Thiago Leite <thiagoleites@gmail.com>
2026-02-20 16:31:43 +0000

    chore: remove php header from one file

--- public/index.php
+++ public/index.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * ===============================================
- * TEMPORARY COMMIT MARKER
- * Branch: lint_tests
- * Date: 2026-02-18
- * Description: Temporary header for CI validation.
- * ===============================================
- */
-
-
 // Inicializar sessão
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
